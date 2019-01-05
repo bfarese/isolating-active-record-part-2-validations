9aa18f32e55ba2fb56b15f8441adcdf27f551a3d
Robert Farese <robertfarese@yahoo.com>
2019-01-05 12:17:57 -0500

    removed additional relation

--- app/models/users/relation.rb
+++ app/models/users/relation.rb
@@ -1,2 +1,11 @@
 class Users::Relation < ApplicationRecord
+  def self.attribute_values(id)
+    name, email = where(id: id).pluck(:name, :email)[0]
+
+    { id: id, name: name, email: email }
+  end
+
+  def self.find_name(id)
+    where(id: id).pluck(:name)[0]
+  end
 end
